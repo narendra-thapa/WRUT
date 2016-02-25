686a9831037e07db13b4848720154476914ed9a7
narendrathapa <thapa.narendra@gmail.com>
2016-02-25 16:46:16 -0500

    AutoLayout of initial screen

--- WRUT/ViewController.swift
+++ WRUT/ViewController.swift
@@ -9,15 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var playerListCollectionView: UICollectionView!
+    @IBOutlet weak var updatesMPCollectionView: UICollectionView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
